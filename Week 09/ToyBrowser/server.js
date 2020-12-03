c9164342df855af23624e665d8fb3b7fd56113c0
袁磊 <yuanlei@yuanleideMacBook-Pro.local>
2020-12-03 08:56:16 +0800

    update

---
+++ Week 09/ToyBrowser/server.js
@@ -0,0 +1,42 @@
+const http = require("http")
+
+http.createServer((req, res) => {
+  let body = [];
+  req.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk.toString());
+  }).on('end', () => {
+    // body = Buffer.concat(body).toString();
+    body = (Buffer.concat([ Buffer.from(body.toString()) ])).toString();
+    console.log(body);
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    // trunk body
+    // res.end('<html lang="en"><head><style>#red {color: red;}</style></head><body><div id="red">Hello World</div></body></html>')
+    res.end(
+`<html maaa=a >
+<head>
+  <style>
+body div #myid{
+  width: 100px;
+  background-color: #ff5000;
+}
+body div img{
+  width: 30px;
+  background-color: #ff1111;
+}
+  </style>
+</head>
+<body>
+  <div>
+    <img id="myid" />
+    <img />
+  </div>
+</body>
+</html>`)
+  });
+}).listen(8088)
+
+console.log(`server is running`);
+
+
