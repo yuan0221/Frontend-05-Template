0bea1f9b2638b58a930c09ece8aec0c07ee8ac04
yl <1452491917@qq.com>
2021-01-15 10:11:45 +0800

    实现手势的逻辑

--- Week 15/Carousel/gesture.js
+++ Week 15/Carousel/gesture.js
@@ -38,18 +38,56 @@ element.addEventListener("touchcancel", event => {
   }
 })
 
+let handler;
+let startX, startY;
+let isPan = false, isPress = false, isTap = true;
+
 let start = (point) => {
-  console.log("start", point.clientX, point.clientY);
+  // console.log("start", point.clientX, point.clientY);
+  isPan = false;
+  isPress = false;
+  isTap = true;
+  startX = point.clientX; startY = point.clientY;
+  handler = setTimeout(() => {
+    isPan = false;
+    isPress = true;
+    isTap = false;
+    handler = null;
+    console.log("press");
+  }, 500);
 }
 
 let move = (point) => {
-  console.log("move", point.clientX, point.clientY);
+  // console.log("move", point.clientX, point.clientY);
+  let dx = point.clientX - startX, dy = point.clientY - startY;
+  if(!isPan && dx ** 2 + dy ** 2 > 100) {
+    isPan = true;
+    isPress = false;
+    isTap = false;
+    clearTimeout(handler);
+    console.log("panstart");
+  }
+
+  if(isPan) {
+    console.log("pan");
+  }
 }
 
 let end = (point) => {
-  console.log("end", point.clientX, point.clientY);
+  // console.log("end", point.clientX, point.clientY);
+  if(isTap) {
+    console.log("tap");
+    clearTimeout(handler);
+  }
+  if(isPress) {
+    console.log("pressend");
+  }
+  if(isPan) {
+    console.log("panend");
+  }
 }
 
 let cancel = (point) => {
-  console.log("cancel", point.clientX, point.clientY);
+  clearTimeout(handler);
+  // console.log("cancel", point.clientX, point.clientY);
 }
