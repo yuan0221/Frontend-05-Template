e88ae507cebf30f06f87670149a07b5459f309c4
yl <1452491917@qq.com>
2021-01-31 19:18:28 +0800

    feat: yeoman生成脚手架

---
+++ Week 17/generator-vue/generators/app/index.js
@@ -0,0 +1,64 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+  }
+
+  async initPackage() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      }
+    ]);
+
+    const pkgJson = {
+      "name": answers.name,
+      "version": "1.0.0",
+      "description": "",
+      "main": "generators/app/index.js",
+      "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+      },
+      "author": "",
+      "license": "ISC",
+      "devDependencies": {
+        
+      },
+      "dependencies": {
+        
+      }
+    };
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+    this.npmInstall(['vue'], { 'save-dev': false });
+    this.npmInstall(['webpack', 'webpack-cli', 'vue-loader', 'vue-template-compiler', 'vue-style-loader',
+    'css-loader', 'copy-webpack-plugin'], { 'save-dev': true });
+
+    this.fs.copyTpl(
+      this.templatePath('HelloWorld.vue'),
+      this.destinationPath('src/HelloWorld.vue'),
+      {}
+    );
+    this.fs.copyTpl(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js'),
+      {}
+    );
+    this.fs.copyTpl(
+      this.templatePath('main.js'),
+      this.destinationPath('src/main.js'),
+      {}
+    );
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('src/index.html'),
+      {title: answers.name}
+    );
+  }
+};
