211f92eb0d56a1753a0a28402b93ab8edde019ab
yl <1452491917@qq.com>
2021-02-05 09:07:51 +0800

    test: 测试html parser

--- Week 18/html-parser/test/parser-test.js
+++ Week 18/html-parser/test/parser-test.js
@@ -2,8 +2,15 @@ var assert = require('assert');
 import {parseHTML} from "../src/parser.js";
 
 describe('parse html:', function() {
-  it('<a>asd</a>', function() {
-    parseHTML('<a>asd</a>');
-    assert.equal(1, 1);
+  it('<a></a>', function() {
+    let tree = parseHTML('<a></a>');
+    assert.equal(tree.children[0].tagName, "a");
+    assert.equal(tree.children[0].children.length, 0);
+  });
+  it('<a href="//time.geekbang.org"></a>', function() {
+    let tree = parseHTML('<a href="//time.geekbang.org"></a>');
+    console.log(tree);
+    assert.equal(tree.children[0].tagName, "a");
+    assert.equal(tree.children[0].children.length, 0);
   });
 })
